166b6e249d61e3b7f5604973b566f650e1fe9bfa
adrubio999 <adrubio1999@gmail.com>
2023-01-10 10:46:44 +0100

    Finished suplementary consensus figure.

--- PaperFigures/FigSupl3.py
+++ PaperFigures/FigSupl3.py
@@ -4,15 +4,18 @@ import seaborn as sns
 import sys
 sys.path.insert(1,'C:\Septiembre-Octubre\Model-Optimization\PaperFigures')
 
-from aux_fcn import fcn_load_pickle,compute_precision_recall_events
+from aux_fcn import fcn_load_pickle
 
 F1_mat=fcn_load_pickle('C:\Septiembre-Octubre\Model-Optimization\PaperFigures\Supl3_consensus_F1.pickle')
 
 arqs=['XGBOOST','SVM','LSTM','CNN2D','CNN1D']
+plt.plot(figsize=(10,10))
 
-f=sns.heatmap(F1_mat, linewidth=0.5,cbar_kws={'label': 'F1'})
+f=sns.heatmap(F1_mat,cmap='viridis', linewidth=0.5,cbar_kws={'label': 'F1'})
 
-
-f.set_yticklabels([1,2,3,4,5])
-f.set_xticklabels(["Best th -0.2","Best th -0.1","Best th","Best th +0.1", "Best th +0.2"],rotation=0)
+#f.set_ytitle("# of positive detections")
+plt.ylabel("# of coincident detectors")
+f.set_yticklabels([5,4,3,2,1])
+f.set_xticklabels(["Best th -0.2","Best th -0.1","Best th","Best th +0.1", "Best th +0.2"],rotation=0,fontsize=8)
+plt.savefig('C:\\Users\\Adrian\\Desktop\\Paper\\Suplementaria consenso.svg')
 plt.show()
